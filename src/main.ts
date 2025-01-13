1b8359f4ae5fbaa6ba644682a61800f622645e71
Dev <aot.dev@gmail.com>
2025-01-13 23:09:14 +0530

    pushing to github

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000','http://13.61.186.57'],
+    origin: ['http://localhost:5173','http://13.61.186.57'],
   })
   await app.listen(process.env.PORT ?? 3000);
 }
