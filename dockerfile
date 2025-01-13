0114cc0350cfc0b4e1a025e5bdc2cb5f963205de
Dev <aot.dev@gmail.com>
2025-01-13 22:34:11 +0530

    port change

--- dockerfile
+++ dockerfile
@@ -8,5 +8,5 @@ COPY . .
 
 # Build and run the app
 RUN npm run build
-EXPOSE 3001
+EXPOSE 3000
 CMD ["npm", "run", "start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','http://13.61.186.57'],
   })
-  await app.listen(process.env.PORT ?? 80);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
